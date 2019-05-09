80743cc2790093c73772aea3814e324178087392
Trishna Chakraborty <trishnachakraborty13@gmail.com>
2019-05-09 12:10:28 +0600

    full flow works

--- SagaOrchestrator.java
+++ SagaOrchestrator.java
@@ -14,7 +14,14 @@ public class SagaOrchestrator {
         events.add("customerBalance");
         try (RPCClient rpcClient = new RPCClient()) {
            rpcClient.getChannel().queueDeclare(rpcClient.getReplyQueueName(), false, false, false, null);
-           String response1 = rpcClient.call(100,"",rpcClient.getReplyQueueName());
+           String response = rpcClient.call("100","customer",rpcClient.getReplyQueueName());
+            System.out.println("Sent message  and get response from customer "+response);
+            rpcClient.call(response,"order",rpcClient.getReplyQueueName());
+            System.out.println("Sent message  and get responsen from order "+response);
+
+
+
+
         } catch (IOException | TimeoutException | InterruptedException e) {
             e.printStackTrace();
         }
